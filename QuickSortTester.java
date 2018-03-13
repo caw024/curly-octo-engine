b202668cb257cfee8b3f0ced4b0c9ed088d989e2
Qian Zhou <qzhou@stuy.edu>
2018-03-13 16:38:16 -0400

    current working version from Taaseen

---
+++ QuickSortTester.java
@@ -0,0 +1,20 @@
+public class QuickSortTester{
+    public static void main(String args[]){
+	double[] results = new double[14];
+	for(int i=6; i<20; i++){
+	    double totalTime = 0;
+	    for(int k=0; k<1000; k++){
+		int[] test = QuickSort.buildArray((int)Math.pow(2,1), 50);
+		double start = System.currentTimeMillis();
+		QuickSort.qsort(test);
+		double end = System.currentTimeMillis();
+		double elapsed = end-start;
+		totalTime += elapsed;
+		System.out.println(totalTime);
+	    }
+	    results[i-6] = totalTime;
+	}
+    }
+}
+
+
