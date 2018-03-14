b7194878a5a8efce23599ed31bf57969e404c237
Taaseen Ali <taaseen.ali89@gmail.com>
2018-03-13 20:55:18 -0400

    Update QuickSortTester.java

--- QuickSortTester.java
+++ QuickSortTester.java
@@ -4,17 +4,19 @@ public class QuickSortTester{
 	for(int i=6; i<20; i++){
 	    double totalTime = 0;
 	    for(int k=0; k<1000; k++){
-		int[] test = QuickSort.buildArray((int)Math.pow(2,1), 50);
+		int[] test = QuickSort.buildArray((int)Math.pow(2,i), 50);
 		double start = System.currentTimeMillis();
 		QuickSort.qsort(test);
 		double end = System.currentTimeMillis();
 		double elapsed = end-start;
 		totalTime += elapsed;
-		System.out.println(totalTime);
+		//System.out.println(totalTime);
 	    }
 	    results[i-6] = totalTime;
+	    System.out.println(totalTime);
 	}
+	for(double d: results) System.out.println(d);
     }
+   
 }
-
 
